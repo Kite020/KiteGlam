0ff98062235b1d9f3181dc1fa5287724956ea51a
91720 <a91720@Ankitas-MacBook-Air.local>
2026-05-12 11:39:35 +0530

    Configured Firebase environment variables

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -21,7 +21,10 @@ const firebaseConfig = {
     process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 
   appId:
-    process.env.REACT_APP_FIREBASE_APP_ID
+    process.env.REACT_APP_FIREBASE_APP_ID,
+
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
